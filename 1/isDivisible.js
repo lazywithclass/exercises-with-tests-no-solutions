191b5dd49f64e5a6f0c15e793c0dfbe4aa450d26
Alberto Zaccagni <montecristoh@gmail.com>
2023-02-08 12:20:14 +0000

    new exercise

---
+++ 1/isDivisible.js
@@ -0,0 +1,9 @@
+// scrivere una funzione chiamata isDivisible che 
+// dato un numero a e un numero b
+// ritorni true se a e' divisibile per b, false altrimenti
+
+function isDivisible(a, b) {
+  return a % b == 0
+}
+
+module.exports = isDivisible
