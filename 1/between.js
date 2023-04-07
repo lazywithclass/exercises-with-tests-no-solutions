29e4cdc0b545e5ee422d7fa5d4115bd40a9d1903
Alberto Zaccagni <montecristoh@gmail.com>
2023-04-07 18:26:23 +0000

    Corretta descrizione

--- 1/between.js
+++ 1/between.js
@@ -1,6 +1,6 @@
 // scrivere una funzione chiamata between che 
 // dati due numeri a e b in ingresso
-// ritorni un array che contiene tutti i numeri compresi tra a e b esclusi
+// ritorni un array che contiene tutti i numeri compresi tra a e b, estremi esclusi
 
 function between(a, b) {
 }
