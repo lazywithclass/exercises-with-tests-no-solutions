22cd85320edafc2879435c868415f7aed4f1508e
lazywithclass <montecristoh@gmail.com>
2023-04-13 19:43:24 +0000

    syntax fix

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const TEST_DA_ESEGUIRE = [
 // i test usano https://jestjs.io/docs/expect
 const testsToExecute = TEST_DA_ESEGUIRE.join('|')
 const { exec } = require('child_process')
-exec(`jest -t '${testsToExecute}' --colors`, (err, stdout, stderr) => {
+exec(`jest -t '${testsToExecute}' --colors`, (err, stdout, stderr) => 
   console.log(stderr)
-})
+)
